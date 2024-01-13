91426c6a83281130c87c74be50fc4b565df30f82
Bill Luong <telleph0ne34@gmail.com>
2024-01-12 16:16:53 -0800

    I'm in

--- main/round.py
+++ main/round.py
@@ -24,12 +24,13 @@ import subprocess
 class round0:
     roundStatus = False
     
-
+    #static --> constant for all rounds
     def __init__(self,roundNumber,scriptD):
         self.roundNumber = roundNumber
         self.name = "round{}".format(self.roundNumber)
         self.directory = "{}/rounds/round{}".format(scriptD, self.roundNumber)
 
+    #static --> constant for all rounds 
     def createImage(self):
         client = docker.from_env()
         client.images.build(path=self.directory, tag=self.name, rm=True)
@@ -37,13 +38,20 @@ class round0:
             detach=True,
             image=self.name,
             command="sleep infinity",
-            name="round0",
+            name=self.name,
             tty=True,
             stdin_open = True
         )
-        gameContainer = client.containers.get("round0").id
-        
-        subprocess.run(['docker', 'exec','-it',gameContainer,'/bin/bash'])
-        #subprocess.run(['docker','enter',gameContainer])
-        print("\n"*2)
-        
+        subprocess.Popen(['docker', 'exec','-it',self.name,'/bin/bash'])
+
+
+    #Will be dynamic for each round
+    def checkSolution(self):
+        ls = subprocess.run(['docker', 'exec','-it','round0','ls'],capture_output=True, text=True)
+        check = subprocess.run(['grep', 'test.txt'],capture_output=True,text=True,input=ls.stderr)
+        print(check.stderr)
+        # if(check.returncode == 1):
+        #     print("YOU FOUND IT")
+        #     #subprocess.run(['docker', 'remove', '--force','round0'])
+        # elif(check.returncode == 0):
+        #     print("test.txt not found try again")
